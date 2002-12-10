aab8d9775dcfce26a0da164249bca52474acf676
Benoit Breholee <benoit@breholee.org>
2002-12-10 15:39:53 +0000

    typo, new constructors/destructor, and conversion functions moved from functions in RTIambassador to methods here

--- libCERTI/GAV.cc
+++ libCERTI/GAV.cc
@@ -19,7 +19,7 @@
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
 // USA
 //
-// $Id: GAV.cc,v 3.1 2002/11/26 15:48:01 breholee Exp $
+// $Id: GAV.cc,v 3.2 2002/12/10 15:39:53 breholee Exp $
 // ---------------------------------------------------------------------------
 
 #include <config.h>
@@ -32,6 +32,93 @@
 namespace certi {
 
   static pdCDebug D("GAV", "(gav)      - ");
+
+//*******************************************************************
+//              Data Integrity
+//*******************************************************************
+//! Returns buffer size needed for storing message restored by stringToObject.
+void 
+getStringToObjectLength(char *init_string, ULong& size)
+{
+    ULong counter = 0;
+    ULong length = strlen(init_string);
+    ULong i = 0;
+    size = 0;
+    while (i<length) {
+        switch (init_string[i]) {
+        case '\\':
+            i++;
+            while ((init_string[i]=='?') &&(i<length)) {
+                counter++;
+                i++;
+            }
+            if ((counter%2)==0) size += (counter/2);
+            else size += 1+((counter-1)/2);            
+            counter = 0;
+            break;
+        default:
+            size++;
+            i++;
+        }
+    }
+}
+
+//! Restore string network message to the original text object.
+/*! stringToObject is used to restore initial object as it was before being
+    sent throw the network. Cases are as follows :
+     - ? : if alone without \ prefixing caracter, restore to \0
+     - \ : this caracter is used to tell that a caracter \ or ? needs replace it
+     - other : keep caracter.
+
+    Pattern is "\???\" for case \. If number of ? is odd, it must be
+    replaced by a list of ? else it must be replaced by a list of \.
+*/
+void 
+stringToObject(char *init_string, char *end_string, ULong size)
+{
+    ULong counter = 0;
+    ULong i = 0;
+    ULong j = 0;
+    ULong indice = 0;
+    ULong length = strlen(init_string);
+    
+    memset(end_string, '\0', size);
+    
+    while(i<length) {
+        switch(init_string[i]) {
+        case '?':
+            // No use to add '\0' since memset does it
+            i++;
+            indice++;
+            break;
+        case '\\':
+            i++;
+            while((init_string[i]=='?') &&(i<length)) {
+                counter++;
+                i++;
+            }
+            i++;
+            if((counter%2)==0) {
+                for(j=0; j<(counter/2); j++) {
+                    end_string[indice]='\\';
+                    indice++;
+                }
+            } 
+            else {
+                for(j=0; j<(1+((counter-1)/2)); j++) {
+                    end_string[indice]='?';
+                    indice++;
+                }
+            }
+            counter = 0;
+            break;
+        default:
+            end_string[indice]=init_string[i];
+            i++;
+            indice++;
+        }
+    }
+}
 
 //-----------------------------------------------------------------------
 // CAttributeHandleValuePair
@@ -47,7 +134,7 @@ CAttributeHandleValuePair::CAttributeHandleValuePair()
   _value.name[0] = '\0';
   _value.value[0] = '\0';
   _value.type[0] = '\0';
-  _value.lenght = 0;
+  _value.length = 0;
   _next =(CAttributeHandleValuePair *)NULL;
 }
 
@@ -76,7 +163,7 @@ CParameterHandleValuePair::CParameterHandleValuePair()
   _value.name[0] = '\0';
   _value.value[0] = '\0';
   _value.type[0] = '\0';
-  _value.lenght = 0;
+  _value.length = 0;
   _next =(CParameterHandleValuePair *)NULL;
 }
 
@@ -94,10 +181,43 @@ void CParameterHandleValuePair::AfficheParameter()
 // CAttributeHandleValuePairSet
 //-----------------------------------------------------------------------
 
-CAttributeHandleValuePairSet::CAttributeHandleValuePairSet()
+CAttributeHandleValuePairSet::CAttributeHandleValuePairSet(void)
+  : _size(0), _head((CAttributeHandleValuePair *)NULL)
 {
-  _size = 0;
-  _head =(CAttributeHandleValuePair *)NULL;
+}
+
+CAttributeHandleValuePairSet::
+CAttributeHandleValuePairSet(const AttributeHandleValuePairSet & ahvps) :
+  _size(0), _head((CAttributeHandleValuePair *)NULL)
+{
+    CAttributeHandleValuePair *cahvp;
+    ULong valueLength;
+
+    for (int i = 0 ; i < ahvps.size() ; i++) {
+        cahvp = new CAttributeHandleValuePair();
+        cahvp->_attrib = ahvps.getHandle(i);
+        ahvps.getValue(i, cahvp->_value.value, valueLength);
+        cahvp->_value.length = valueLength;
+        add(cahvp);
+    }
+}
+
+CAttributeHandleValuePairSet::
+CAttributeHandleValuePairSet(const AttributeHandleSet & ahs)
+  : _size(0), _head((CAttributeHandleValuePair *)NULL)
+{
+  CAttributeHandleValuePair *cahvp;
+
+  for (int i = 0 ; i < ahs.size() ; i++) {
+    cahvp = new CAttributeHandleValuePair();
+    cahvp->_attrib = ahs.getHandle(i);
+    add(cahvp);
+  }
+}
+
+CAttributeHandleValuePairSet::~CAttributeHandleValuePairSet(void)
+{
+    empty();
 }
 
 void CAttributeHandleValuePairSet::add(CAttributeHandleValuePair *att)
@@ -156,8 +276,7 @@ getIeme(UShort pos) const
 void CAttributeHandleValuePairSet::del(AttributeHandle attrib)
 {
   CAttributeHandleValuePair *tmp = _head;
-  if(tmp->_attrib == attrib)
-    {
+  if(tmp->_attrib == attrib) {
       _head = _head->_next;
       delete tmp;
       _size--;
@@ -198,14 +317,69 @@ void CAttributeHandleValuePairSet::empty()
   assert(_size == 0);
 }
 
+AttributeHandleValuePairSet* 
+CAttributeHandleValuePairSet::toAHVPS(void) const
+{
+    ULong longueur;
+    CAttributeHandleValuePair *cahvp;
+    
+    AttributeHandleValuePairSet *ahvps;
+    ahvps = AttributeSetFactory::create(_size);
+    
+    for(int i = 0 ; i<_size ; i++) {
+        cahvp = getIeme(i);
+        
+        if(cahvp != NULL) {
+            if(&cahvp->_value != NULL) {
+                // decodage
+                getStringToObjectLength(cahvp->_value.value,longueur);
+                char *valeur = new char[longueur];
+                valeur[0] = '\0' ;
+                stringToObject(cahvp->_value.value,valeur,longueur);
+                memcpy(cahvp->_value.value, valeur, longueur);
+                /*  phvps->add(cahvp->_param,
+                    cahvp->_value.value,
+                    (strlen(cahvp->_value.value)*sizeof(char)) );*/
+                
+                ahvps->add(cahvp->_attrib, cahvp->_value.value, longueur);
+            }
+            else ahvps->add(cahvp->_attrib,'\0', 0);            
+        }
+    }
+
+  return ahvps;
+}
+
 //-----------------------------------------------------------------------
 // CParameterHandleValuePairSet
 //-----------------------------------------------------------------------
 
-CParameterHandleValuePairSet::CParameterHandleValuePairSet()
+CParameterHandleValuePairSet::CParameterHandleValuePairSet(void)
+  : _size(0), _head((CParameterHandleValuePair *)NULL)
+
 {
-  _size = 0;
-  _head =(CParameterHandleValuePair *)NULL;
+}
+
+CParameterHandleValuePairSet::
+CParameterHandleValuePairSet(const ParameterHandleValuePairSet & phvps)
+  : _size(0), _head((CParameterHandleValuePair *)NULL)
+
+{
+    CParameterHandleValuePair *cphvp = NULL;
+    ULong valueLength = 0;
+    
+    for (ulong i = 0 ; i < phvps.size() ; i++) {
+        cphvp = new CParameterHandleValuePair();
+        cphvp->_param = phvps.getHandle(i);
+        phvps.getValue(i, cphvp->_value.value, valueLength);
+        cphvp->_value.length = valueLength;
+        add(cphvp);
+    }
+}
+
+CParameterHandleValuePairSet::~CParameterHandleValuePairSet(void)
+{
+    empty();
 }
 
 void CParameterHandleValuePairSet::add(CParameterHandleValuePair *par)
@@ -304,6 +478,37 @@ void CParameterHandleValuePairSet::empty()
       // assert(_size >= 0);  FIXME: était toujours positif(unsigned)
     }
   assert(_size == 0);
+}
+
+ParameterHandleValuePairSet* 
+CParameterHandleValuePairSet::toPHVPS(void) const
+{
+    ULong longueur;
+    CParameterHandleValuePair *cphvp;
+    
+    ParameterHandleValuePairSet *phvps;
+    phvps = ParameterSetFactory::create(_size);
+    
+    for(int i = 0 ; i<_size ; i++) {
+        cphvp = getIeme(i);
+        
+        if(cphvp != NULL) {
+            if(&cphvp->_value != NULL) {
+
+                // decoding string to object
+                getStringToObjectLength(cphvp->_value.value,longueur);
+                char *valeur = new char[longueur];
+                valeur[0] = '\0' ;
+                stringToObject(cphvp->_value.value,valeur,longueur);
+                memcpy(cphvp->_value.value, valeur, longueur);
+                
+                // adding new value to PHVPS
+                phvps->add(cphvp->_param, cphvp->_value.value, longueur );	
+            }
+            else phvps->add(cphvp->_param,'\0', 0);
+        }
+    }
+    return phvps;
 }
 
 // //-----------------------------------------------------------------------
@@ -829,4 +1034,4 @@ void CParameterHandleValuePairSet::empty()
 
 }
 
-// $Id: GAV.cc,v 3.1 2002/11/26 15:48:01 breholee Exp $
+// $Id: GAV.cc,v 3.2 2002/12/10 15:39:53 breholee Exp $
