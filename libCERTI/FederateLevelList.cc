2a14bac7869ce0a12cdc05936b6771bbf8a7a7eb
Benoit Breholee <benoit@breholee.org>
2003-01-15 18:08:30 +0000

    copyright update

--- libCERTI/FederateLevelList.cc
+++ libCERTI/FederateLevelList.cc
@@ -1,16 +1,16 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*- 
 // ---------------------------------------------------------------------------
 // CERTI - HLA RunTime Infrastructure
-// Copyright (C) 2002  ONERA
+// Copyright (C) 2002, 2003  ONERA
 //
-// This file is part of CERTI-libcerti
+// This file is part of CERTI-libCERTI
 //
-// CERTI-libcerti is free software; you can redistribute it and/or
+// CERTI-libCERTI is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public License
 // as published by the Free Software Foundation; either version 2 of
 // the License, or (at your option) any later version.
 //
-// CERTI-libcerti is distributed in the hope that it will be useful, but
+// CERTI-libCERTI is distributed in the hope that it will be useful, but
 // WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 // Lesser General Public License for more details.
@@ -20,7 +20,7 @@
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
 // USA
 //
-// $Id: FederateLevelList.cc,v 3.2 2003/01/15 18:02:22 breholee Exp $
+// $Id: FederateLevelList.cc,v 3.3 2003/01/15 18:08:30 breholee Exp $
 // ---------------------------------------------------------------------------
 
 #include <config.h>
@@ -72,4 +72,4 @@ FederateLevelList::getLevel(const char* theName) const
 }
 }
 
-// $Id: FederateLevelList.cc,v 3.2 2003/01/15 18:02:22 breholee Exp $
+// $Id: FederateLevelList.cc,v 3.3 2003/01/15 18:08:30 breholee Exp $
