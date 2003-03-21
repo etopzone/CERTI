30eae0920e65a2fde2ec35ef46df71475b58a087
Benoit Breholee <benoit@breholee.org>
2003-03-21 18:18:48 +0000

    added ostreams

--- libpdebug/PrettyDebug.cc
+++ libpdebug/PrettyDebug.cc
@@ -20,7 +20,7 @@
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
 // USA
 //
-// $Id: PrettyDebug.cc,v 3.3 2003/03/21 18:22:12 breholee Exp $
+// $Id: PrettyDebug.cc,v 3.2 2003/03/21 18:18:48 breholee Exp $
 // ----------------------------------------------------------------------------
 
 #include "PrettyDebug.hh"
@@ -28,16 +28,22 @@
 DebugOStream DebugOStream::nullOutputStream(cout);
 DebugOStream PrettyDebug::defaultOutputStream(cerr);
 
+#ifdef TEST_DEBUGOSTREAM_PTR
 //Fix this pointer to the default initialisation of pointers for your compiler
 DebugOStream* PrettyDebug::nullOutputStreamPtr = 0 ; 
 DebugOStream& PrettyDebug::nullOutputStream = DebugOStream::nullOutputStream;
+#else
+DebugOStream* PrettyDebug::nullOutputStreamPtr = &(DebugOStream::nullOutputStream);
+#endif
+
+//Private
 
 // ----------------------------------------------------------------------------
 /** Print the message to the default output ostream.
  *  This function does NOT add any trailing \n. */
 void
 PrettyDebug::Print(DebugOStream& theOutputStream,
-                   const char* theHeaderMessage, const char * Message)
+                const char* theHeaderMessage, const char * Message)
 {
     if (Message != NULL)
         theOutputStream << theHeaderMessage << Message;
@@ -215,4 +221,4 @@ PrettyDebug::Out(pdDebugLevel Level, const char * Format, ...)
 
 #endif // NO_PRETTYDEBUG
 
-// $Id: PrettyDebug.cc,v 3.3 2003/03/21 18:22:12 breholee Exp $
+// $Id: PrettyDebug.cc,v 3.2 2003/03/21 18:18:48 breholee Exp $
