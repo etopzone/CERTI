569e744be694d68ae9a1fff090eb83a1c658d897
Benoit Breholee <benoit@breholee.org>
2005-05-06 13:38:42 +0000

    bugfix

--- libCERTI/HandleManager.hh
+++ libCERTI/HandleManager.hh
@@ -1,15 +1,13 @@
 // ----------------------------------------------------------------------------
 // CERTI - HLA RunTime Infrastructure
-// Copyright (C) 2002, 2003, 2005  Benoît Bréholée
+// Copyright (C) 2002-2005  Benoît Bréholée
 //
-// This file is part of CERTI-libCERTI
-//
-// CERTI-libCERTI is free software ; you can redistribute it and/or
+// This program is free software ; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public License
 // as published by the Free Software Foundation ; either version 2 of
 // the License, or (at your option) any later version.
 //
-// CERTI-libCERTI is distributed in the hope that it will be useful, but
+// This program is distributed in the hope that it will be useful, but
 // WITHOUT ANY WARRANTY ; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 // Lesser General Public License for more details.
@@ -19,7 +17,7 @@
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
 // USA
 //
-// $Id: HandleManager.hh,v 3.0 2005/03/25 16:07:31 breholee Exp $
+// $Id: HandleManager.hh,v 3.1 2005/05/06 13:38:42 breholee Exp $
 // ----------------------------------------------------------------------------
 
 #ifndef LIBCERTI_HANDLE_MANAGER
@@ -40,7 +38,7 @@ class HandleManager
 public:
     HandleManager(T);
     HandleManager(T, size_t);
-    T provide() throw (RTIinternalError);
+    T provide();
     void free(T);
 
 private:
@@ -49,8 +47,7 @@ private:
     std::list<T> available ;
 };
 
-// FUNCTIONS DEFINITION
-
+// ============================================================================
 /** Constructor
     @param init Initial handle value (use for first requested handle)
     @param hmax Maximal handle value
@@ -66,23 +63,22 @@ template<typename T>
 HandleManager<T>::HandleManager(T init)
     : maximum(std::numeric_limits<T>::max()), highest(init) { }
 
+// ----------------------------------------------------------------------------
 /** Get a new handle
     @return handle
     @throw RTIinternalError if all handles between first and maximal are used
  */
-template<typename T>
-T
+template<typename T> T
 HandleManager<T>::provide()
-    throw (RTIinternalError)
 {
-    T handle ;
+    T handle = 0 ;
 
     if (available.size() > 0) {
 	handle = available.front();
 	available.pop_front();
     }
     else {
-	if (handle < maximum)
+	if (highest < maximum)
 	    handle = highest++ ;
 	else
 	    throw RTIinternalError("Maximum handle reached");
@@ -91,12 +87,12 @@ HandleManager<T>::provide()
     return handle ;
 }
 
+// ----------------------------------------------------------------------------
 /** Free a handle
     @pre handle is a previously-provided handle
     @param handle Handle to free
  */
-template<typename T>
-void
+template<typename T> void
 HandleManager<T>::free(T handle)
 {
     if (handle + 1 == highest)
