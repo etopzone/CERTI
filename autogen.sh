991973d46e640394b1649bf3916bfcb04a012180
Benoit Breholee <benoit@breholee.org>
2006-05-14 17:41:09 +0000

    replace bootstrap by autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,13 @@
 #! /bin/sh
+# CERTI bootstrap
+# $Id: autogen.sh,v 1.1.2.1 2006/05/14 17:41:09 breholee Exp $
 
-echo CERTI `cat VERSION` ...
+echo [bootstrapping CERTI `cat VERSION` ...]
 set -x
 libtoolize --force --copy
 aclocal
 autoheader
 automake --add-missing --copy
 autoconf
+
+# $Id: autogen.sh,v 1.1.2.1 2006/05/14 17:41:09 breholee Exp $
