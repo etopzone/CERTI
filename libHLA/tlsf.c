90f693eef21babf7b2bdd9f9e1b3f4ceb74f8e95
Eric Noulard <eric.noulard@gmail.com>
2010-05-31 13:58:09 +0000

    Use unamed variadadic macro There is still a warning due to -pedantic

--- libHLA/tlsf.c
+++ libHLA/tlsf.c
@@ -173,14 +173,14 @@
 
 #ifdef USE_PRINTF
 #include <stdio.h>
-# define PRINT_MSG(fmt, args...) printf(fmt, ## args)
-# define ERROR_MSG(fmt, args...) fprintf(stderr, fmt, ## args)
+# define PRINT_MSG(...) printf(__VA_ARGS__)
+# define ERROR_MSG(...) fprintf(stderr, __VA_ARGS__)
 #else
 # if !defined(PRINT_MSG)
-#  define PRINT_MSG(fmt, args...)
+#  define PRINT_MSG(...)
 # endif
 # if !defined(ERROR_MSG)
-#  define ERROR_MSG(fmt, args...)
+#  define ERROR_MSG(...)
 # endif
 #endif
 
