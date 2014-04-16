aef985f935ea98d9da17da5b6a1210a2e1c032bd
Eric Noulard <eric.noulard@gmail.com>
2014-04-16 11:30:49 +0000

    Avoid forward declaration of auto_ptr. Use proper standard include header <memory>.

--- include/ieee1516-2010/RTI/certiLogicalTimeFactory.h
+++ include/ieee1516-2010/RTI/certiLogicalTimeFactory.h
@@ -31,14 +31,10 @@ namespace rti1516e
   class LogicalTimeInterval;
 }
 
-namespace std
-{
-  template <class T> class auto_ptr;
-}
-
 #include <RTI/SpecificConfig.h>
 #include <RTI/Exception.h>
 #include <string>
+#include <memory>
 
 // LogicalTimeFactory is used by the RTI to construct instances of classes
 // derived from LogicalTime and LogicalTimeInterval.  A federation is responsible
