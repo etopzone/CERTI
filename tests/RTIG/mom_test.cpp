2651bb212e6341cbc76f7bd7bae92b2766b299e2
Clément Vannier <clement.vannier@scalian.com>
2017-10-31 10:42:10 +0100

    RTIG: Add a test class for MOM implementation
    Clément Vannier for Scalian

---
+++ tests/RTIG/mom_test.cpp
@@ -0,0 +1,443 @@
+#include <gtest/gtest.h>
+
+TEST(MomTest, 11_1_1_The_RTI_shall_publish_object_classes)
+{
+    // Test after setup that the mom published
+}
+
+TEST(MomTest, 11_1_2_The_RTI_shall_register_object_instances_and_update_values_of_attributes)
+{
+    // Test after setup that the mom registered one instance of federation
+}
+
+TEST(MomTest, 11_1_3_The_RTI_shall_subscribe_to_and_receive_some_interaction_classes)
+{
+    
+}
+
+TEST(MomTest, 11_1_4_The_RTI_shall_publish_and_send_other_interaction_classes)
+{
+    
+}
+
+TEST(MomTest, 11_1_5_All_MOM_items_shall_be_predefined_in_the_FDD)
+{
+    // Test with valid and invalids xml and assert that mom_enabled is good
+}
+
+TEST(MomTest, 11_1_6_Those_definitions_shall_not_be_revised)
+{
+    // Test that incomplete feds are detected
+}
+
+TEST(MomTest, 11_1_7_MOM_definitions_may_be_extended_and_augmented)
+{
+    // items can be added without jeoparding mom enabling
+}
+
+TEST(MomTest, 11_1_8_These_new_elements_shall_not_be_acted_upon_directly_by_the_RTI)
+{
+    // by default the RTI does not care about those new item. how to test ?
+}
+
+TEST(MomTest, 11_2_a_1_The_RTI_shall_publish_and_register_one_instance_of_manager_federate_per_federate)
+{
+    
+}
+
+TEST(MomTest, 11_2_a_2_The_RTI_shall_update_the_information_based_on_federate_adjust)
+{
+    
+}
+
+TEST(MomTest, 11_2_a_3_Information_shall_be_contained_in_an_object_instance)
+{
+    
+}
+
+TEST(MomTest, 11_2_b_The_RTI_shall_publish_and_register_one_instance_of_manager_federation_per_federation)
+{
+    
+}
+
+TEST(MomTest, 11_3_a_The_RTI_shall_act_upon_receiving_adjust_interaction)
+{
+    
+}
+
+TEST(MomTest, 11_3_b_The_RTI_shall_act_upon_receiving_request_interaction)
+{
+    
+}
+/*
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+TEST(MomTest, The_RTI_shall_)
+{
+    
+}
+
+*/
