dacffa2131d505c103981c1e225016790b28e9fc
Clément Vannier <clement.vannier@scalian.com>
2017-12-04 19:23:45 +0100

    LibRTI: Add call to base class ctor in copy ctor
    Clément Vannier for Scalian

--- libRTI/ieee1516-2010/HandleImplementation.cpp
+++ libRTI/ieee1516-2010/HandleImplementation.cpp
@@ -21,28 +21,25 @@
 //
 // ----------------------------------------------------------------------------
 
-#include <RTI/Handle.h>
-#include <climits>
-#include <sstream>
-#include <cstring>
-#include <stdint.h>
-#include <functional>
 #include "HandleImplementation.h"
 #include "RTIvariableLengthDataImplementation.h"
+#include <RTI/Handle.h>
+#include <climits>
+#include <cstring>
+#include <functional>
+#include <sstream>
+#include <stdint.h>
 
 #include "PMurHash.h"
 
-namespace rti1516e
-{
+namespace rti1516e {
 
 /* Constructs an invalid handle                           */
-HandleImplementation::HandleImplementation()
-: _value(ULONG_MAX)
+HandleImplementation::HandleImplementation() : _value(ULONG_MAX)
 {
 }
 
-HandleImplementation::HandleImplementation(VariableLengthData const & encodedValue)
-: _value(ULONG_MAX)
+HandleImplementation::HandleImplementation(VariableLengthData const& encodedValue) : _value(ULONG_MAX)
 {
     ULong val = 0;
     const size_t size = sizeof(val);
@@ -53,9 +50,8 @@ HandleImplementation::HandleImplementation(VariableLengthData const & encodedVal
     }
 
     memcpy(&val, encodedValue.data(), size);
-    for(::uint32_t i = 0; i < sizeof(val); i++)
-    {
-        buf[size-i-1] = (char) val & 0xFF;
+    for (::uint32_t i = 0; i < sizeof(val); i++) {
+        buf[size - i - 1] = (char) val & 0xFF;
         val = val >> 8;
     }
 
@@ -65,13 +61,11 @@ HandleImplementation::HandleImplementation(VariableLengthData const & encodedVal
     _value = newValue;
 }
 
-HandleImplementation::~HandleImplementation()
-throw()
+HandleImplementation::~HandleImplementation() throw()
 {
 }
 
-HandleImplementation::HandleImplementation(HandleImplementation const & rhs)
-: _value(ULONG_MAX)
+HandleImplementation::HandleImplementation(HandleImplementation const& rhs) : _value(ULONG_MAX)
 {
     _value = rhs._value;
 }
@@ -85,8 +79,7 @@ bool HandleImplementation::isValid() const
         return true;
 }
 
-
-long HandleImplementation::hash () const
+long HandleImplementation::hash() const
 {
     long retval;
     //from <functional>
@@ -98,14 +91,14 @@ long HandleImplementation::hash () const
     // Use Portable MurmurHash implementation from http://code.google.com/p/smhasher/
     std::wstring strval = toString();
     // if long a 32 bits integer then hash is computed in one piece
-    if (sizeof(long)==sizeof(uint32_t)) {
-        retval = PMurHash32(0,strval.c_str(),strval.length());
+    if (sizeof(long) == sizeof(uint32_t)) {
+        retval = PMurHash32(0, strval.c_str(), strval.length());
     }
     // otherwise long is 64 bits so hash should be computed in two 32 bits pieces
     else {
         int l = strval.length();
-        retval = PMurHash32(0,strval.c_str(),l/2);
-        retval |= (retval >> 32) | PMurHash32(0,strval.c_str()+l/2,l-l/2);
+        retval = PMurHash32(0, strval.c_str(), l / 2);
+        retval |= (retval >> 32) | PMurHash32(0, strval.c_str() + l / 2, l - l / 2);
     }
     return retval;
 }
@@ -125,18 +118,15 @@ unsigned long HandleImplementation::encodedLength() const
 {
     return sizeof(_value);
 }
-unsigned long HandleImplementation::encode(
-        void* buffer, unsigned long bufferSize) const
-throw (CouldNotEncode)
+unsigned long HandleImplementation::encode(void* buffer, unsigned long bufferSize) const throw(CouldNotEncode)
 {
     if (bufferSize < sizeof(_value))
         throw CouldNotEncode(L"Not enough room in buffer to encode handle");
 
     unsigned long val = _value;
-    char *buf = (char *) buffer;
-    for(uint32_t i = 0; i < sizeof(_value); i++)
-    {
-        buf[sizeof(_value)-i-1] = (char) val & 0xFF;
+    char* buf = (char*) buffer;
+    for (uint32_t i = 0; i < sizeof(_value); i++) {
+        buf[sizeof(_value) - i - 1] = (char) val & 0xFF;
         val = val >> 8;
     }
     return sizeof(_value);
@@ -149,57 +139,49 @@ std::wstring HandleImplementation::toString() const
     return ost.str();
 }
 
-
-#define DECLARE_HANDLE_IMPLEMENTATION_CLASS(HIK)                 \
-        \
-        /* Constructs an invalid handle                           */ \
-        HIK::HIK()                                                   \
-        : HandleImplementation()                                     \
-          {                                                            \
-          }                                                            \
-          \
-          HIK::HIK(VariableLengthData const & encodedValue)            \
-          : HandleImplementation(encodedValue)                         \
-            {                                                            \
-              \
-            }                                                            \
-            \
-            HIK::~HIK()                                                  \
-            throw()                                                      \
-            {                                                            \
-            }                                                            \
-            \
-            HIK::HIK(HIK const & rhs)                                    \
-            {                                                            \
-                _value = rhs._value;									 \
-            }                                                            \
-            \
-            HIK & HIK::operator=(HIK const & rhs)                        \
-            {                                                            \
-                if (this != &rhs)                                        \
-                {                                                        \
-                    _value = rhs._value;                                 \
-                }                                                        \
-                return *this;                                            \
-            }                                                            \
-            \
-            \
-            /* All invalid handles are equivalent                     */ \
-        bool HIK::operator==(HIK const & rhs) const                  \
-        {                                                            \
-                return _value == rhs._value;                             \
-        }                                                            \
-        bool HIK::operator!=(HIK const & rhs) const                  \
-        {                                                            \
-            return _value != rhs._value;                             \
-        }                                                            \
-        bool HIK::operator< (HIK const & rhs) const                  \
-        {                                                            \
-            return _value < rhs._value;                              \
-        }                                                            \
-        \
-        /* end DECLARE_HANDLE_IMPLEMENTATION_CLASS */
-
+#define DECLARE_HANDLE_IMPLEMENTATION_CLASS(HIK)                                                                       \
+                                                                                                                       \
+    /* Constructs an invalid handle                           */                                                       \
+    HIK::HIK() : HandleImplementation()                                                                                \
+    {                                                                                                                  \
+    }                                                                                                                  \
+                                                                                                                       \
+    HIK::HIK(VariableLengthData const& encodedValue) : HandleImplementation(encodedValue)                              \
+    {                                                                                                                  \
+    }                                                                                                                  \
+                                                                                                                       \
+    HIK::~HIK() noexcept                                                                                               \
+    {                                                                                                                  \
+    }                                                                                                                  \
+                                                                                                                       \
+    HIK::HIK(HIK const& rhs) : HandleImplementation()                                                                  \
+    {                                                                                                                  \
+        _value = rhs._value;                                                                                           \
+    }                                                                                                                  \
+                                                                                                                       \
+    HIK& HIK::operator=(HIK const& rhs)                                                                                \
+    {                                                                                                                  \
+        if (this != &rhs) {                                                                                            \
+            _value = rhs._value;                                                                                       \
+        }                                                                                                              \
+        return *this;                                                                                                  \
+    }                                                                                                                  \
+                                                                                                                       \
+    /* All invalid handles are equivalent                     */                                                       \
+    bool HIK::operator==(HIK const& rhs) const                                                                         \
+    {                                                                                                                  \
+        return _value == rhs._value;                                                                                   \
+    }                                                                                                                  \
+    bool HIK::operator!=(HIK const& rhs) const                                                                         \
+    {                                                                                                                  \
+        return _value != rhs._value;                                                                                   \
+    }                                                                                                                  \
+    bool HIK::operator<(HIK const& rhs) const                                                                          \
+    {                                                                                                                  \
+        return _value < rhs._value;                                                                                    \
+    }                                                                                                                  \
+                                                                                                                       \
+/* end DECLARE_HANDLE_IMPLEMENTATION_CLASS */
 
 DECLARE_HANDLE_IMPLEMENTATION_CLASS(FederateHandleImplementation)
 DECLARE_HANDLE_IMPLEMENTATION_CLASS(ObjectClassHandleImplementation)
@@ -211,36 +193,34 @@ DECLARE_HANDLE_IMPLEMENTATION_CLASS(DimensionHandleImplementation)
 //DECLARE_HANDLE_IMPLEMENTATION_CLASS(MessageRetractionHandleImplementation)
 DECLARE_HANDLE_IMPLEMENTATION_CLASS(RegionHandleImplementation)
 
-
-MessageRetractionHandleImplementation::MessageRetractionHandleImplementation() 
-: HandleImplementation()
+MessageRetractionHandleImplementation::MessageRetractionHandleImplementation() : HandleImplementation()
 {
 }
 
-MessageRetractionHandleImplementation::MessageRetractionHandleImplementation(MessageRetractionHandleImplementation const & rhs)
-{                                                        
+MessageRetractionHandleImplementation::MessageRetractionHandleImplementation(
+    MessageRetractionHandleImplementation const& rhs)
+{
     _value = rhs._value;
 }
 
-MessageRetractionHandleImplementation::MessageRetractionHandleImplementation(VariableLengthData const & encodedValue)
-{                                                            
+MessageRetractionHandleImplementation::MessageRetractionHandleImplementation(VariableLengthData const& encodedValue)
+{
     ULong val1 = 0;
     ULong val2 = 0;
     const size_t size = sizeof(val1);
     unsigned char buf1[size];
     unsigned char buf2[size];
 
-    if (encodedValue.size() != 2*size) {
+    if (encodedValue.size() != 2 * size) {
         throw CouldNotDecode(L"Encoded value has an unexpected size.");
     }
 
     memcpy(&val1, encodedValue.data(), size);
-    memcpy(&val2, (ULong*)encodedValue.data() + 1 , size);
+    memcpy(&val2, (ULong*) encodedValue.data() + 1, size);
 
     // _value
-    for(uint32_t i = 0; i < sizeof(val1); i++)
-    {
-        buf1[size-i-1] = (char) val1 & 0xFF;
+    for (uint32_t i = 0; i < sizeof(val1); i++) {
+        buf1[size - i - 1] = (char) val1 & 0xFF;
         val1 = val1 >> 8;
     }
     //copy buf1 to _value
@@ -249,9 +229,8 @@ MessageRetractionHandleImplementation::MessageRetractionHandleImplementation(Var
     _value = newValue;
 
     // _serialNum
-    for(uint32_t i = 0; i < sizeof(val2); i++)
-    {
-        buf2[size-i-1] = (char) val2 & 0xFF;
+    for (uint32_t i = 0; i < sizeof(val2); i++) {
+        buf2[size - i - 1] = (char) val2 & 0xFF;
         val2 = val2 >> 8;
     }
     //copy buf2 to _serailNum
@@ -260,12 +239,12 @@ MessageRetractionHandleImplementation::MessageRetractionHandleImplementation(Var
     _serialNum = newSerialNum;
 }
 
-MessageRetractionHandleImplementation::~MessageRetractionHandleImplementation()                                                
-throw()
+MessageRetractionHandleImplementation::~MessageRetractionHandleImplementation() throw()
 {
 }
 
-MessageRetractionHandleImplementation & MessageRetractionHandleImplementation::operator=(MessageRetractionHandleImplementation const & rhs)
+MessageRetractionHandleImplementation& MessageRetractionHandleImplementation::
+operator=(MessageRetractionHandleImplementation const& rhs)
 {
     if (this != &rhs) {
         _value = rhs._value;
@@ -273,61 +252,57 @@ MessageRetractionHandleImplementation & MessageRetractionHandleImplementation::o
     return *this;
 }
 
-bool MessageRetractionHandleImplementation::operator==(MessageRetractionHandleImplementation const & rhs) const                  
-        {
+bool MessageRetractionHandleImplementation::operator==(MessageRetractionHandleImplementation const& rhs) const
+{
     bool isEqual = (_value == rhs._value) && (_serialNum == rhs._serialNum);
     return isEqual;
-        }
+}
 
-bool MessageRetractionHandleImplementation::operator!=(MessageRetractionHandleImplementation const & rhs) const                  
-        {
+bool MessageRetractionHandleImplementation::operator!=(MessageRetractionHandleImplementation const& rhs) const
+{
     bool isEqual = (_value == rhs._value) && (_serialNum == rhs._serialNum);
     return !isEqual;
-        }
+}
 
-bool MessageRetractionHandleImplementation::operator< (MessageRetractionHandleImplementation const & rhs) const                  
-{                                                            
+bool MessageRetractionHandleImplementation::operator<(MessageRetractionHandleImplementation const& rhs) const
+{
     return _value < rhs._value;
-} 
+}
 
-/* Generate an encoded value that can be used to send     */ 
-/* handles to other federates in updates or interactions. */ 
-VariableLengthData MessageRetractionHandleImplementation::encode() const                       
-{                                                            
-    unsigned char buf[ sizeof(_value) + sizeof(_serialNum) ];
-    encode(buf, sizeof(_value) + sizeof(_serialNum) );
-    VariableLengthData c(buf, sizeof(_value) + sizeof(_serialNum) );
+/* Generate an encoded value that can be used to send     */
+/* handles to other federates in updates or interactions. */
+VariableLengthData MessageRetractionHandleImplementation::encode() const
+{
+    unsigned char buf[sizeof(_value) + sizeof(_serialNum)];
+    encode(buf, sizeof(_value) + sizeof(_serialNum));
+    VariableLengthData c(buf, sizeof(_value) + sizeof(_serialNum));
     return c;
-}                                                            
+}
 
-/* Alternate encode for directly filling a buffer         */ 
-unsigned long MessageRetractionHandleImplementation::encodedLength() const                     
-{                                                            
-    return sizeof(_value) + sizeof(_serialNum);                                   
-}    
+/* Alternate encode for directly filling a buffer         */
+unsigned long MessageRetractionHandleImplementation::encodedLength() const
+{
+    return sizeof(_value) + sizeof(_serialNum);
+}
 
-unsigned long MessageRetractionHandleImplementation::encode(                                   
-        void* buffer, unsigned long bufferSize) const
-throw (CouldNotEncode)                                       
-{                                                            
-    if ( bufferSize < (sizeof(_value)+sizeof(_serialNum)) )                         
+unsigned long MessageRetractionHandleImplementation::encode(void* buffer, unsigned long bufferSize) const
+    throw(CouldNotEncode)
+{
+    if (bufferSize < (sizeof(_value) + sizeof(_serialNum)))
         throw CouldNotEncode(L"Not enough room in buffer to encode handle");
 
     unsigned long val = _value;
     unsigned long serialNum = _serialNum;
-    char *buf = (char *) buffer;
-    for(uint32_t i = 0; i < sizeof(_value); i++)
-    {
-        buf[sizeof(_value)-i-1] = (char) val & 0xFF;
+    char* buf = (char*) buffer;
+    for (uint32_t i = 0; i < sizeof(_value); i++) {
+        buf[sizeof(_value) - i - 1] = (char) val & 0xFF;
         val = val >> 8;
     }
-    for(uint32_t i = 0; i < sizeof(_serialNum); i++)
-    {
-        buf[sizeof(_value)+sizeof(_serialNum)-i-1] = (char) serialNum & 0xFF;
+    for (uint32_t i = 0; i < sizeof(_serialNum); i++) {
+        buf[sizeof(_value) + sizeof(_serialNum) - i - 1] = (char) serialNum & 0xFF;
         serialNum = serialNum >> 8;
     }
     return sizeof(_value) + sizeof(_serialNum);
-} 
-
+}
 
 } // end namespace rti1516
